0ba83268b4762c483d4dc6e51d04c6c73ea2dc2d
Baizid MD Ashadzzaman <86706204+Baizidmdashadzzaman@users.noreply.github.com>
2024-03-28 15:03:19 +0600

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import AnalogClockLib from "@/masjid-admin-panel/components2/lib/AnalogClockLib.vue";
+</script>
+
+<template>
+  <AnalogClockLib :size="400" timeFormat="24hour" hourFormat="standard" />
+</template>
